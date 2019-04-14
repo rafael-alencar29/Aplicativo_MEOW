1d37efafd61c728f606375d9ab4be4426f8ba858
Rafael <rafael.alencar29@gmail.com>
2019-04-13 21:07:36 -0300

    Initial commit

---
+++ app/src/main/java/com/example/meow/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.meow;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int WELCOME_TIMEOUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent welcome = new Intent(MainActivity.this, activity2.class);
+                startActivity(welcome);
+
+                overridePendingTransition(R.anim.fadein,R.anim.fadeout);
+                finish();
+            }
+        },WELCOME_TIMEOUT);
+
+    }
+}
