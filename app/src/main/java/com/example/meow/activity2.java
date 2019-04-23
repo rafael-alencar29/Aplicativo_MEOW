8d65a2326054e92d31688a2a1f3cf7f4b1a8d2a1
Rafael <rafael.alencar29@gmail.com>
2019-04-23 14:18:07 -0300

    Telas inicias do aplicativo

--- app/src/main/java/com/example/meow/activity2.java
+++ app/src/main/java/com/example/meow/activity2.java
@@ -1,13 +1,29 @@
 package com.example.meow;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class activity2 extends AppCompatActivity {
+
+    Button adotarButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity2);
+
+        adotarButton = findViewById(R.id.adotarButton);
+        adotarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(activity2.this, com.example.meow.adotar.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
