21c79f927c203998f4abeb62fa355874d263796e
axelmar16 <marcelo.acn@hotmail.com>
2019-05-03 03:00:49 -0300

    Adicionar imagem ao cadastro de Animal

--- app/src/main/java/com/example/meow/cadastroAnimal.java
+++ app/src/main/java/com/example/meow/cadastroAnimal.java
@@ -1,10 +1,26 @@
 package com.example.meow;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class cadastroAnimal extends AppCompatActivity {
+
+    private ImageView imagem;
+    private final int GALERIA_IMAGENS = 1;
+    private final int PERMISSION_REQUEST = 2;
 
 
     @Override
@@ -12,7 +28,53 @@ public class cadastroAnimal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastroanimal);
 
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            } else {
+                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST);
+            }
+        }
 
 
+        imagem = (ImageView) findViewById(R.id.imageView10);
+        final Button galeria = (Button) findViewById(R.id.btnImagem);
+        galeria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                galeria.setVisibility(View.INVISIBLE);
+                startActivityForResult(intent, GALERIA_IMAGENS);
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == GALERIA_IMAGENS) {
+            Uri selectedImage = data.getData();
+            String[] filePath = { MediaStore.Images.Media.DATA };
+            Cursor c = getContentResolver().query(selectedImage, filePath, null, null, null);
+            c.moveToFirst();
+            int columnIndex = c.getColumnIndex(filePath[0]);
+            String picturePath = c.getString(columnIndex);
+            c.close();
+            Bitmap imagemGaleria = (BitmapFactory.decodeFile(picturePath));
+            imagem.setImageBitmap(imagemGaleria);
+        }
+    }
+
+    // faz o tratamento da resposta de permissao do usuario
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permission[], int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            } else {
+
+            }
+        }
+    }
+
+
 }
